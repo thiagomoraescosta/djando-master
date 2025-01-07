021a64fb8b76159f5d1eb48394f803988d322bc4
Thiago Moraes <133033871+thiagomoraescosta@users.noreply.github.com>
2025-01-06 22:52:40 -0300

    Operadores Logicos

---
+++ operadoresLogicos.py
@@ -0,0 +1,18 @@
+nome = 'Thiago'
+idade = 39
+peso = 91
+# or == ou (apenas uma condição precisa ser true)
+# != (diferente) / == (igual)
+if (nome == 'Thiago' or idade == 25) and peso != 90:
+    print('Dados corretos')
+else:
+    print('Dados incorretos')
+
+# and == e ( as duas condições tem que ser True)
+cidade = 'Lins'
+estado = 'São Paulo'
+
+if cidade == 'Sabino' and estado == 'São Paulo':
+    print('Dados corretos')
+else:
+    print('Dados incorretos')
